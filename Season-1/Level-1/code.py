4cdb5b675adbf30d9e192e42041dbda94a72cf3e
Sota Sugiura <2968860+sota1235@users.noreply.github.com>
2024-03-12 13:12:13 +0000

    modify: clear Season-1 Level-1

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,20 +12,26 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import *
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
     net = 0
+    amount = 0
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            net = Decimal(str(net)) + Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            net = Decimal(str(net)) - Decimal(str(item.amount)) * item.quantity
+            amount += Decimal(str(item.amount)) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
+        
+    if amount > 100000:
+        return 'Total amount payable for an order exceeded'
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
